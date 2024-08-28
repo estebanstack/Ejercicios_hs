706ee0091143a622c7e73569eef8ddea7dd0e357
estebanstack <130319203+estebanstack@users.noreply.github.com>
2024-08-28 08:15:08 -0500

    Delete ejercicio12.hs

--- ejercicio12.hs
+++
@@ -1,8 +0,0 @@
-removeDuplicates :: (Eq a) => [a] -> [a]
-removeDuplicates [] = []
-removeDuplicates (x:xs)
-    | x `elem` xs = removeDuplicates xs
-    | otherwise = x : removeDuplicates xs
-    
-main :: IO ()
-main = print(removeDuplicates [1, 2, 3, 2, 5])
